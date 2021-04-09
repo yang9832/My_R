c224f5ffa37faaec4e167fa84b654be5586a4feb
yang9832 <82200760+yang9832@users.noreply.github.com>
2021-04-09 14:40:48 +0800

    homework of week03

---
+++ hw/week03/hw_03.r
@@ -0,0 +1,29 @@
+library(tidyverse)
+
+dt <- tibble::tribble(
+  ~id, ~num,
+  1, 6,
+  2, 5,
+  3, 4,
+  4, 3,
+  5, 2,
+  6, 2,
+  7, 1
+)
+dt
+color = c('#668B24','#C3C4BF','#E15241','#D4C479','#0E8AAC',
+          '#A4D291','#CF5D05')
+setwd('D:/Learn/R/hw/hw_03')
+img <- png::readPNG('lego_example.png')
+
+ggplot(dt) +
+  ggpubr::background_image(img) +
+  geom_col(aes(x=id,y=num),fill=color,width=1) + 
+  xlim(-1,9) + 
+  ylim(-2,7) + 
+  theme(panel.background=element_rect(colour = "#BCB5AD"),
+        axis.text.x = element_blank(),
+        axis.text.y = element_blank()) +
+  labs(x = NULL, y = NULL)
+  
+
