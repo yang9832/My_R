f704ce3cd30fd9a6678ab318e12e0f9ddf5f62af
yang9832 <82200760+yang9832@users.noreply.github.com>
2021-04-13 16:30:04 +0800

    Update 01_group_by_and_aggregate.R

--- daily/01_group_by_and_aggregate.R
+++ daily/01_group_by_and_aggregate.R
@@ -4,13 +4,19 @@ attach(mtcars)
 
 # 整合数据(aggregate)
 options(digits=3)
-(aggdata <- aggregate(mtcars, by=list(cyl,gear), FUN=mean, na.rm=TRUE))
+#FUN=mean 使用平均函数(mean)进行处理
+(aggdata <- aggregate(mtcars, by=list(cyl,gear), FUN=mean, na.rm=TRUE)) 
+
 
 
 
 # 整合数据(group_by)
 mtcars %>%
   group_by(cyl,gear) %>%
-  summarise(across(everything(), ~ round(mean(.),2)))
+  summarise(
+    across(
+      everything(), ~ round(mean(.),2) # round()设置结果的小数位
+      )
+    )
 
             
